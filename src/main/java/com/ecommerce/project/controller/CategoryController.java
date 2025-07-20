fe3a615570c43dc42d1e80c7335980aefbb71a4d
ANUKULJHA1506 <studious.anukul@gmail.com>
2025-07-20 23:30:47 +0530

    Pagination, Sorting and APIResponse is done

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.project.controller;
 
 
+import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
@@ -30,8 +31,14 @@ public class CategoryController {
 
 
     @GetMapping("/public/categories")
-public ResponseEntity<CategoryResponse> getAllCategories() {
-    CategoryResponse categoryResponse = categoryService.getAllCategories();
+public ResponseEntity<CategoryResponse> getAllCategories(
+        @RequestParam(name = "pageNumber",defaultValue = AppConstants.PAGE_NUMBER,required = false)Integer pageNumber,
+        @RequestParam(name = "pageSize",defaultValue = AppConstants.PAGE_SIZE,required = false)Integer pageSize,
+        @RequestParam(name = "sortBy",defaultValue = AppConstants.SORT_CATEGORIES_BY) String sortBy,
+        @RequestParam(name = "sortOrder",defaultValue = AppConstants.SORT_DIR) String sortOrder
+
+    ) {
+    CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize,sortBy,sortOrder);
     return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
@@ -43,11 +50,9 @@ public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDT
 }
 
     @DeleteMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
-
-
-        String status = categoryService.deleteCategory(categoryId);
-        return new ResponseEntity<>(status, HttpStatus.OK);
+    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId) {
+        CategoryDTO deleteCategory = categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(deleteCategory, HttpStatus.OK);
 
     }
 
